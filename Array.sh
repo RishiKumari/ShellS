fbdc6a1e6b2ca1325cf4b24a023eeaeb8d1ec7b5
Rishika Kumari <78131848+RishiKumari@users.noreply.github.com>
2022-03-30 11:50:14 +0530

    30/03/2022

---
+++ Array.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+NAME[0]="Rishika"
+NAME[1]="Shibuu"
+NAME[2]="Nikuu"
+NAME[3]="Teena"
+NAME[4]="Ayaan"
+#printing  all the values of array
+echo "First Method: ${NAME[*]}"
+echo "Second Method: ${NAME[@]}"
